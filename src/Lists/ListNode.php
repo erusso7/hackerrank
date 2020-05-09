05133a1f4011d714feb9eac1ffe3f4c463e1629c
Ezequiel Russo <ezequiel.russo@typeform.com>
2020-05-09 11:16:18 +0200

    Add missing ListNode

---
+++ src/Lists/ListNode.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+namespace App\Lists;
+
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+
+    function __construct($val = 0, $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
